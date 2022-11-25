939628efcd0cae9131ad6a75adb8edec26730260
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-11-24 17:48:43 -0800

    added pairs of songs problem

---
+++ solutions/PairsOfSongs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        d = [0]*60
+        result = 0
+        for i in time:
+            duration = i % 60
+            nduration = duration if duration else 60
+            result += d[60-nduration]     
+            d[duration] += 1
+        return result
